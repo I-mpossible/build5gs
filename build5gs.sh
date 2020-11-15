ffb515064c159cc6081ab38470cb7994a1d2df0d
I-mpossible <582932205@qq.com>
2020-11-15 15:33:39 +0800

    initial

--- build5gs.sh
+++ build5gs.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 sudo sh -c "echo 'nameserver 8.8.8.8' > /etc/resolv.conf"
 sudo apt update
-sudo apt install mongodb
+sudo apt install -y mongodb
 sudo systemctl start mongodb
 sudo systemctl enable mongodb
 
-sudo apt install python3-pip python3-setuptools python3-wheel ninja-build build-essential flex bison git libsctp-dev libgnutls28-dev libgcrypt-dev libssl-dev libidn11-dev libmongoc-dev libbson-dev libyaml-dev libmicrohttpd-dev libcurl4-gnutls-dev meson
+sudo apt install -y python3-pip python3-setuptools python3-wheel ninja-build build-essential flex bison git libsctp-dev libgnutls28-dev libgcrypt-dev libssl-dev libidn11-dev libmongoc-dev libbson-dev libyaml-dev libmicrohttpd-dev libcurl4-gnutls-dev meson
 
 git clone https://github.com/open5gs/open5gs
 cd open5gs
